0293adb40906cb79f4e67653cda4bfebb3675857
shubh sahu <shubhsahu2006@gmail.com>
2026-02-17 23:23:31 +0530

    changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://stock-wise-inventory-management-bac.vercel.app/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://stock-wise-inventory-management-backend-rifk4er5q.vercel.app/api'
 })
 
 api.interceptors.request.use((config) => {
